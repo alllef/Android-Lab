db7759632d1adb125bd07b33e7f002e0eb0c6444
alllef <alekz29@gmail.com>
2021-11-24 21:21:51 +0200

    Added functionality for second fragment. Doesn't work properly

--- app/src/main/java/com/github/alllef/androidlab1/MainActivity.kt
+++ app/src/main/java/com/github/alllef/androidlab1/MainActivity.kt
@@ -5,39 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     // private lateinit var binding: ResultProfileBinding
-
     lateinit var radioGroup: RadioGroup
     lateinit var okButton: Button
-    lateinit var cancelButton: Button
     lateinit var editText: EditText
-    lateinit var textView: TextView
     lateinit var radioGroupManager: RadioGroupManager
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, EditTextFragment())
+            .commit()
+
         okButton = findViewById(R.id.ok)
-        cancelButton = findViewById(R.id.cancel)
         editText = findViewById(R.id.text_to_edit)
-        textView = findViewById(R.id.converted_text)
         radioGroupManager = RadioGroupManager(findViewById(R.id.group))
 
         okButton.setOnClickListener {
             onOkButtonClicked()
         }
-
-        cancelButton.setOnClickListener {
-            onCancelButtonClicked()
-        }
-    }
-
-    private fun onCancelButtonClicked() {
-        textView.text = ""
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -51,6 +43,21 @@ class MainActivity : AppCompatActivity() {
             else -> println("what is it")
         }
 
+        val textViewFragment: Fragment = TextViewFragment()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, textViewFragment)
+            .commit()
+
+        val cancelButton: Button = findViewById(R.id.cancel)
+
+        cancelButton.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .remove(EditTextFragment())
+                .commit()
+        }
+
+        val textView: TextView = findViewById(R.id.converted_text)
         textView.typeface = resources.getFont(fontId as Int)
         textView.text = editText.text
     }
--- app/src/main/java/com/github/alllef/androidlab1/TextViewFragment.kt
+++ app/src/main/java/com/github/alllef/androidlab1/TextViewFragment.kt
@@ -7,5 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 
 class TextViewFragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_text_view, container, false)
+    }
 }
